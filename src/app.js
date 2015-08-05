075f5bba5897b6088cc05384fc6eaa33d4ce4d4b
Christoph Werner <christoph@codepunkt.de>
2015-08-05 12:00:50 +0200

    Simplify, performance optimize and replace react-document-title with react-helmet

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React from 'react/addons';
-import { Link, Router, Route } from 'react-router';
+import { Link, Router, Route, Redirect } from 'react-router';
 import BrowserHistory from 'react-router/lib/BrowserHistory'
-import DocumentTitle from 'react-document-title';
+import Helmet from 'react-helmet';
 import './app.css';
 
 // @todo alias to nothing with webpack in production
@@ -18,18 +18,13 @@ class Header extends React.Component {
             <nav className="menu col-xs-9" role="navigation">
               <ul className="menu_list">
                 <li className="menu_item">
-                  <Link className="menu_link" activeClassName="menu_link-active" to="/">
-                    Home
+                  <Link className="menu_link" activeClassName="menu_link-active" to="/" aria-label="Visit the home page">
+                    Shop
                   </Link>
                 </li>
                 <li className="menu_item">
-                  <Link className="menu_link" activeClassName="menu_link-active" to="/first">
-                    Page #1
-                  </Link>
-                </li>
-                <li className="menu_item">
-                  <Link className="menu_link" activeClassName="menu_link-active" to="/second">
-                    Page #2
+                  <Link className="menu_link" activeClassName="menu_link-active" to="/cart" aria-label="Show your shopping cart">
+                    Cart
                   </Link>
                 </li>
               </ul>
@@ -52,9 +47,11 @@ class App extends React.Component {
 
   render() {
     return (
-      <React.addons.TransitionGroup>
-        {this.renderChildren()}
-      </React.addons.TransitionGroup>
+      <Helmet titleTemplate="%s | Metrolines">
+        <React.addons.TransitionGroup>
+          {this.renderChildren()}
+        </React.addons.TransitionGroup>
+      </Helmet>
     );
   }
 }
@@ -63,22 +60,23 @@ class App extends React.Component {
 
 class Page extends React.Component {
   render() {
-    let className = this.props.className ? `page ${this.props.className}` : 'page';
+    let className = this.props.className
+      ? `page ${this.props.className}`
+      : 'page';
 
     return (
-      <DocumentTitle title={this.props.title}>
-        <div className={className}>
-          <Header />
-          <main className="main" role="main">
-            <div className="container">{this.props.children}</div>
-          </main>
-        </div>
-      </DocumentTitle>
+      <div className={className}>
+        <Helmet title={this.props.title} />
+        <Header />
+        <main className="main" role="main">
+          <div className="container">{this.props.children}</div>
+        </main>
+      </div>
     );
   }
 }
 
-function animate() {
+function animate(prefix) {
   return Component => class Animate extends Component {
     static displayName = `Animate(${Component.displayName || Component.name})`;
 
@@ -87,58 +85,56 @@ function animate() {
       this.state = { className: '' };
     }
 
-    transition(name, callback) {
-      const TICK = 17;
-      React.findDOMNode(this).addEventListener('transitionend', callback);
-      setTimeout((() => this.setState({ className: `page-${name}` })), TICK);
+    applyTransitionClass(className, callback) {
+      let node = React.findDOMNode(this);
+      let onTransitionEnd = (() => {
+        node.removeEventListener('transitionend', onTransitionEnd);
+        callback();
+      });
+
+      node.addEventListener('transitionend', onTransitionEnd);
+      window.setTimeout(() => this.setState({
+        className: `${prefix}-${className}`
+      }));
     }
 
     componentWillAppear(callback) {
-      this.transition('appear', callback);
+      this.applyTransitionClass('appear', callback);
     }
 
     componentWillEnter(callback) {
-      this.transition('enter', callback);
+      this.applyTransitionClass('enter', callback);
     }
 
     componentWillLeave(callback) {
-      this.transition('leave', callback);
+      this.applyTransitionClass('leave', callback);
     }
 
     render() {
-      return <Component className={this.state.className} />;
+      return (
+        <Component className={this.state.className} />
+      );
     }
   };
 }
 
-@animate()
-class HomePage extends React.Component {
+@animate('page')
+class Shop extends React.Component {
   render() {
     return (
-      <Page title="Home Page" {...this.props}>
-        Home Page
+      <Page title="Shop" {...this.props}>
+        Shop
       </Page>
     );
   }
 }
 
-@animate()
-class FirstPage extends React.Component {
+@animate('page')
+class Cart extends React.Component {
   render() {
     return (
-      <Page title="First Page" {...this.props}>
-        First Page
-      </Page>
-    );
-  }
-}
-
-@animate()
-class SecondPage extends React.Component {
-  render() {
-    return (
-      <Page title="Second Page" {...this.props}>
-        Second Page
+      <Page title="Cart" {...this.props}>
+        Cart
       </Page>
     );
   }
@@ -147,9 +143,9 @@ class SecondPage extends React.Component {
 React.render((
   <Router history={new BrowserHistory}>
     <Route component={App}>
-      <Route path="/" component={HomePage} />
-      <Route path="/first" component={FirstPage} />
-      <Route path="/second" component={SecondPage} />
+      <Route path="/" component={Shop} />
+      <Route path="cart" component={Cart} />
+      <Redirect from="*" to="/" />
     </Route>
   </Router>
 ), document.body);
